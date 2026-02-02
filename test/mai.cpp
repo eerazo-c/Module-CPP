dbeba53a64dc58c2f0c5cbc39dafc93991a22505
Elizabeth Nayeli Erazo Chacon <elerazo-@car10s1.42barcelona.com>
2026-02-02 20:15:23 +0100

    done cp00

---
+++ test/mai.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+   std::cout << "Introduce un entero: ";
+   int x;
+   std::cin >> x;
+   std::cout << "Introduce otro entero: ";
+   int y;
+   std::cin >> y;
+   std::cout << "x=" << x << "\ny=" << y << '\n';
+}
