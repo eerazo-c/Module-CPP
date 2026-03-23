bee63c9917d55cbc709f80583bdc2866de00507e
Eli Erazo <tucorreo@gmail.com>
2026-03-23 20:09:26 +0100

    start7

---
+++ cpp07/ex00/inc/whatever.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elerazo- <elerazo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/13 18:40:41 by elerazo-          #+#    #+#             */
+/*   Updated: 2026/03/13 18:41:05 by elerazo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#pragma once
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <iomanip>
+
+class  ScalarConverter
+{
+	private:
+		ScalarConverter();
+		~ScalarConverter();
+		ScalarConverter(const ScalarConverter &obj);
+		ScalarConverter &operator=(const ScalarConverter &obj);
+
+	public:
+		static void convert(std::string type);
+};
+
+typedef void(*f_form)(std::string type);
+
+#endif
---
+++ cpp07/ex00/src/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elerazo- <elerazo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 15:32:45 by elerazo-          #+#    #+#             */
+/*   Updated: 2026/03/18 15:35:08 by elerazo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ScalarConverter.hpp"
+
+int main( void )
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
